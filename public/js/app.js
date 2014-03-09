929f5691b102cced7bddb649606b5da85ab4a5ee
Cole Miller <millercolin@mac.com>
2014-03-09 17:04:24 -0400

    ng-fantasyfootball
    Original thinkster.io AngularJS tutorial w/ MEAN stack

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,10 @@
-window.app = angular.module('ngFantasyFootball', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ngRoute', 'ngff.controllers', 'ngff.directives', 'ngff.services']);
+// ng modules
+window.app = angular.module('ngFantasyFootball', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ngRoute', 'ngff.controllers', 'ngff.directives', 'ngff.services']); // alternative to using 'ng-app' directive in head/html
 
 // bundling dependencies
-window.angular.module('ngff.controllers', ['ngff.controllers.header','ngff.controllers.index']);
-window.angular.module('ngff.services', ['ngff.services.global']);
+
+// ng controllers
+window.angular.module('ngff.controllers', ['ngff.controllers.header','ngff.controllers.index','ngff.controllers.nfl','ngff.controllers.leagues','ngff.controllers.fantasyTeams','ngff.controllers.players']);
+
+// ng services
+window.angular.module('ngff.services', ['ngff.services.global','ngff.services.nfl','ngff.services.leagues','ngff.services.fantasyTeams','ngff.services.players']);
